162bb925687c57b0349e719b824c9c69e8992c53
aliahmadcse <aliahmadcse@gmail.com>
2020-03-07 23:25:49 +0500

    Loads server side content with ajax

--- Chapter_03/03_02/ajax_html/new_content.php
+++ Chapter_03/03_02/ajax_html/new_content.php
@@ -1,1 +1,7 @@
-This is the new content which has been loaded by Ajax.
+<?php
+$var = "new content";
+?>
+
+<span style="color:aqua">
+    This is the <strong><?= $var ?></strong> which has been loaded by Ajax.
+</span>
