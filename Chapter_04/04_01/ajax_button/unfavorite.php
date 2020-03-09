08812db70f1eeb61a01c306c0f3b1c3d8fb42485
aliahmadcse <aliahmadcse@gmail.com>
2020-03-10 01:39:32 +0500

    Adds feature to un-favorite a post

---
+++ Chapter_04/04_01/ajax_button/unfavorite.php
@@ -0,0 +1,38 @@
+<?php
+// You can simulate a slow server with sleep
+// sleep(2);
+
+session_start();
+
+if (!isset($_SESSION['favorites'])) {
+    $_SESSION['favorites'] = [];
+}
+
+function is_ajax_request()
+{
+    return isset($_SERVER['HTTP_X_REQUESTED_WITH']) &&
+        $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest';
+}
+
+if (!is_ajax_request()) {
+    exit;
+}
+
+// extract $id
+$raw_id = isset($_POST['id']) ? $_POST['id'] : '';
+
+// echo $raw_id;
+if (preg_match("/blog-post-(\d+)/", $raw_id, $matches)) {
+    $id = $matches[1];
+    // store in $_SESSION['favorites']
+    if (in_array($id, $_SESSION['favorites'])) {
+        $item_index = array_search($id, $_SESSION['favorites']);
+        unset($_SESSION['favorites'][$item_index]);
+    }
+
+    echo "true";
+} else {
+    echo "false";
+}
+
+  // return true/false
