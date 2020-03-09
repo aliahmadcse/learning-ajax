20243fb5afa1fb0637583dece5dde8eb9988e60a
aliahmadcse <aliahmadcse@gmail.com>
2020-03-10 00:38:15 +0500

    Parse the ajax post request on the server and sends response back

--- Chapter_04/04_01/ajax_button/favorite.php
+++ Chapter_04/04_01/ajax_button/favorite.php
@@ -1,24 +1,37 @@
 <?php
-  // You can simulate a slow server with sleep
-  // sleep(2);
+// You can simulate a slow server with sleep
+// sleep(2);
 
-  session_start();
+session_start();
 
-  if(!isset($_SESSION['favorites'])) { $_SESSION['favorites'] = []; }
+if (!isset($_SESSION['favorites'])) {
+  $_SESSION['favorites'] = [];
+}
 
-  function is_ajax_request() {
-    return isset($_SERVER['HTTP_X_REQUESTED_WITH']) &&
-      $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest';
+function is_ajax_request()
+{
+  return isset($_SERVER['HTTP_X_REQUESTED_WITH']) &&
+    $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest';
+}
+
+if (!is_ajax_request()) {
+  exit;
+}
+
+// extract $id
+$raw_id = isset($_POST['id']) ? $_POST['id'] : '';
+
+// echo $raw_id;
+if (preg_match("/blog-post-(\d+)/", $raw_id, $matches)) {
+  $id = $matches[1];
+  // store in $_SESSION['favorites']
+  if (!in_array($id, $_SESSION['favorites'])) {
+    $_SESSION['favorites'][] = $id;
   }
 
-  if(!is_ajax_request()) { exit; }
+  echo "true";
+} else {
+  echo "false";
+}
 
-  // extract $id
-  $raw_id = isset($_POST['id']) ? $_POST['id'] : '';
-
-  echo $raw_id;
-  
-  // store in $_SESSION['favorites']
   // return true/false
-
-?>
