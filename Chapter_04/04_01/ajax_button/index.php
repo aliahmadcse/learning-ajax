247ff353e793361e7fbb2f1c39e36b169c66f992
aliahmadcse <aliahmadcse@gmail.com>
2020-03-10 00:53:57 +0500

    Adds heart to favorite post

--- Chapter_04/04_01/ajax_button/index.php
+++ Chapter_04/04_01/ajax_button/index.php
@@ -33,23 +33,37 @@ if (!isset($_SESSION['favorites'])) {
     button.favorite-button:hover {
       background: #000099;
     }
+
+    .favorite-heart {
+      display: none;
+      font-size: 2rem;
+      color: red;
+      float: right
+    }
+
+    .favorite .favorite-heart {
+      display: block;
+    }
   </style>
 </head>
 
 <body>
-  <?php echo join(',', $_SESSION['favorites']) ?>
+  <!-- <?php echo join(',', $_SESSION['favorites']) ?> -->
   <div id="blog-posts">
     <div id="blog-post-101" class="blog-post">
+      <span class="favorite-heart">&hearts;</span>
       <h3>Blog Post 101</h3>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed scelerisque nunc malesuada mauris fermentum commodo. Integer non pellentesque augue, vitae pellentesque tortor. Ut gravida ullamcorper dolor, ac fringilla mauris interdum id. Nulla porta egestas nisi, et eleifend nisl tincidunt suscipit. Suspendisse massa ex, fringilla quis orci a, rhoncus porta nulla. Aliquam diam velit, bibendum sit amet suscipit eget, mollis in purus. Sed mattis ultricies scelerisque. Integer ligula magna, feugiat non purus eget, pharetra volutpat orci. Duis gravida neque erat, nec venenatis dui dictum vel. Maecenas molestie tortor nec justo porttitor, in sagittis libero consequat. Maecenas finibus porttitor nisl vitae tincidunt.</p>
       <button class="favorite-button">Favorite</button>
     </div>
     <div id="blog-post-102" class="blog-post">
+      <span class="favorite-heart">&hearts;</span>
       <h3>Blog Post 102</h3>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed scelerisque nunc malesuada mauris fermentum commodo. Integer non pellentesque augue, vitae pellentesque tortor. Ut gravida ullamcorper dolor, ac fringilla mauris interdum id. Nulla porta egestas nisi, et eleifend nisl tincidunt suscipit. Suspendisse massa ex, fringilla quis orci a, rhoncus porta nulla. Aliquam diam velit, bibendum sit amet suscipit eget, mollis in purus. Sed mattis ultricies scelerisque. Integer ligula magna, feugiat non purus eget, pharetra volutpat orci. Duis gravida neque erat, nec venenatis dui dictum vel. Maecenas molestie tortor nec justo porttitor, in sagittis libero consequat. Maecenas finibus porttitor nisl vitae tincidunt.</p>
       <button class="favorite-button">Favorite</button>
     </div>
     <div id="blog-post-103" class="blog-post">
+      <span class="favorite-heart">&hearts;</span>
       <h3>Blog Post 103</h3>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed scelerisque nunc malesuada mauris fermentum commodo. Integer non pellentesque augue, vitae pellentesque tortor. Ut gravida ullamcorper dolor, ac fringilla mauris interdum id. Nulla porta egestas nisi, et eleifend nisl tincidunt suscipit. Suspendisse massa ex, fringilla quis orci a, rhoncus porta nulla. Aliquam diam velit, bibendum sit amet suscipit eget, mollis in purus. Sed mattis ultricies scelerisque. Integer ligula magna, feugiat non purus eget, pharetra volutpat orci. Duis gravida neque erat, nec venenatis dui dictum vel. Maecenas molestie tortor nec justo porttitor, in sagittis libero consequat. Maecenas finibus porttitor nisl vitae tincidunt.</p>
       <button class="favorite-button">Favorite</button>
@@ -68,6 +82,9 @@ if (!isset($_SESSION['favorites'])) {
         if (xhr.readyState == 4 && xhr.status == 200) {
           var result = xhr.responseText;
           console.log('Result: ' + result);
+          if (result == "true") {
+            parent.classList.add('favorite');
+          }
         }
       };
       xhr.send("id=" + parent.id);
