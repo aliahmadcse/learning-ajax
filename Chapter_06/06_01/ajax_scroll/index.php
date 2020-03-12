9f718abe86ebf02325c1ce58bc7371a91b079c0f
aliahmadcse <aliahmadcse@gmail.com>
2020-03-13 03:19:48 +0500

    Adds infinite scroll project

--- Chapter_06/06_01/ajax_scroll/index.php
+++ Chapter_06/06_01/ajax_scroll/index.php
@@ -1,96 +1,100 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>Infinite Scroll</title>
-    <style>
-      #blog-posts {
-        width: 700px;
-      }
-      .blog-post {
-        border: 1px solid black;
-        margin: 10px 10px 20px 10px;
-        padding: 6px 10px;
-      }
-      #spinner {
-        display: none;
-      }
-    </style>
-  </head>
-  <body>
-    <div id="blog-posts">
-      <div id="blog-post-101" class="blog-post">
-        <h3>Blog Post 101</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed scelerisque nunc malesuada mauris fermentum commodo. Integer non pellentesque augue, vitae pellentesque tortor. Ut gravida ullamcorper dolor, ac fringilla mauris interdum id. Nulla porta egestas nisi, et eleifend nisl tincidunt suscipit. Suspendisse massa ex, fringilla quis orci a, rhoncus porta nulla. Aliquam diam velit, bibendum sit amet suscipit eget, mollis in purus. Sed mattis ultricies scelerisque. Integer ligula magna, feugiat non purus eget, pharetra volutpat orci. Duis gravida neque erat, nec venenatis dui dictum vel. Maecenas molestie tortor nec justo porttitor, in sagittis libero consequat. Maecenas finibus porttitor nisl vitae tincidunt.</p>
-      </div>
-      <div id="blog-post-102" class="blog-post">
-        <h3>Blog Post 102</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed scelerisque nunc malesuada mauris fermentum commodo. Integer non pellentesque augue, vitae pellentesque tortor. Ut gravida ullamcorper dolor, ac fringilla mauris interdum id. Nulla porta egestas nisi, et eleifend nisl tincidunt suscipit. Suspendisse massa ex, fringilla quis orci a, rhoncus porta nulla. Aliquam diam velit, bibendum sit amet suscipit eget, mollis in purus. Sed mattis ultricies scelerisque. Integer ligula magna, feugiat non purus eget, pharetra volutpat orci. Duis gravida neque erat, nec venenatis dui dictum vel. Maecenas molestie tortor nec justo porttitor, in sagittis libero consequat. Maecenas finibus porttitor nisl vitae tincidunt.</p>
-      </div>
-      <div id="blog-post-103" class="blog-post">
-        <h3>Blog Post 103</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed scelerisque nunc malesuada mauris fermentum commodo. Integer non pellentesque augue, vitae pellentesque tortor. Ut gravida ullamcorper dolor, ac fringilla mauris interdum id. Nulla porta egestas nisi, et eleifend nisl tincidunt suscipit. Suspendisse massa ex, fringilla quis orci a, rhoncus porta nulla. Aliquam diam velit, bibendum sit amet suscipit eget, mollis in purus. Sed mattis ultricies scelerisque. Integer ligula magna, feugiat non purus eget, pharetra volutpat orci. Duis gravida neque erat, nec venenatis dui dictum vel. Maecenas molestie tortor nec justo porttitor, in sagittis libero consequat. Maecenas finibus porttitor nisl vitae tincidunt.</p>
-      </div>
+
+<head>
+  <meta charset="utf-8">
+  <title>Infinite Scroll</title>
+  <style>
+    #blog-posts {
+      width: 700px;
+    }
+
+    .blog-post {
+      border: 1px solid black;
+      margin: 10px 10px 20px 10px;
+      padding: 6px 10px;
+    }
+
+    #spinner {
+      display: none;
+    }
+  </style>
+</head>
+
+<body>
+  <div id="blog-posts">
+    <div id="blog-post-101" class="blog-post">
+      <h3>Blog Post 101</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed scelerisque nunc malesuada mauris fermentum commodo. Integer non pellentesque augue, vitae pellentesque tortor. Ut gravida ullamcorper dolor, ac fringilla mauris interdum id. Nulla porta egestas nisi, et eleifend nisl tincidunt suscipit. Suspendisse massa ex, fringilla quis orci a, rhoncus porta nulla. Aliquam diam velit, bibendum sit amet suscipit eget, mollis in purus. Sed mattis ultricies scelerisque. Integer ligula magna, feugiat non purus eget, pharetra volutpat orci. Duis gravida neque erat, nec venenatis dui dictum vel. Maecenas molestie tortor nec justo porttitor, in sagittis libero consequat. Maecenas finibus porttitor nisl vitae tincidunt.</p>
     </div>
-
-    <div id="spinner">
-      <img src="spinner.gif" width="50" height="50" />
+    <div id="blog-post-102" class="blog-post">
+      <h3>Blog Post 102</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed scelerisque nunc malesuada mauris fermentum commodo. Integer non pellentesque augue, vitae pellentesque tortor. Ut gravida ullamcorper dolor, ac fringilla mauris interdum id. Nulla porta egestas nisi, et eleifend nisl tincidunt suscipit. Suspendisse massa ex, fringilla quis orci a, rhoncus porta nulla. Aliquam diam velit, bibendum sit amet suscipit eget, mollis in purus. Sed mattis ultricies scelerisque. Integer ligula magna, feugiat non purus eget, pharetra volutpat orci. Duis gravida neque erat, nec venenatis dui dictum vel. Maecenas molestie tortor nec justo porttitor, in sagittis libero consequat. Maecenas finibus porttitor nisl vitae tincidunt.</p>
     </div>
-
-    <div id="load-more-container">
-      <button id="load-more">Load more</button>
+    <div id="blog-post-103" class="blog-post">
+      <h3>Blog Post 103</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed scelerisque nunc malesuada mauris fermentum commodo. Integer non pellentesque augue, vitae pellentesque tortor. Ut gravida ullamcorper dolor, ac fringilla mauris interdum id. Nulla porta egestas nisi, et eleifend nisl tincidunt suscipit. Suspendisse massa ex, fringilla quis orci a, rhoncus porta nulla. Aliquam diam velit, bibendum sit amet suscipit eget, mollis in purus. Sed mattis ultricies scelerisque. Integer ligula magna, feugiat non purus eget, pharetra volutpat orci. Duis gravida neque erat, nec venenatis dui dictum vel. Maecenas molestie tortor nec justo porttitor, in sagittis libero consequat. Maecenas finibus porttitor nisl vitae tincidunt.</p>
     </div>
+  </div>
 
-    <script>
+  <div id="spinner">
+    <img src="spinner.gif" width="50" height="50" />
+  </div>
 
-      var container = document.getElementById('blog-posts');
-      var load_more = document.getElementById('load-more');
+  <div id="load-more-container">
+    <button id="load-more">Load more</button>
+  </div>
 
-      function showSpinner() {
-        var spinner = document.getElementById("spinner");
-        spinner.style.display = 'block';
-      }
+  <script>
+    var container = document.getElementById('blog-posts');
+    var load_more = document.getElementById('load-more');
 
-      function hideSpinner() {
-        var spinner = document.getElementById("spinner");
-        spinner.style.display = 'none';
-      }
+    function showSpinner() {
+      var spinner = document.getElementById("spinner");
+      spinner.style.display = 'block';
+    }
 
-      function showLoadMore() {
-        load_more.style.display = 'inline';
-      }
+    function hideSpinner() {
+      var spinner = document.getElementById("spinner");
+      spinner.style.display = 'none';
+    }
 
-      function hideLoadMore() {
-        load_more.style.display = 'none';
-      }
+    function showLoadMore() {
+      load_more.style.display = 'inline';
+    }
 
-      function loadMore() {
+    function hideLoadMore() {
+      load_more.style.display = 'none';
+    }
 
-        showSpinner();
-        hideLoadMore();
+    function loadMore() {
 
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', 'blog_posts.php?page=1', true);
-        xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-        xhr.onreadystatechange = function () {
-          if(xhr.readyState == 4 && xhr.status == 200) {
-            var result = xhr.responseText;
-            console.log('Result: ' + result);
+      showSpinner();
+      hideLoadMore();
 
-            hideSpinner();
-            // append results to end of blog posts
-            showLoadMore();
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', 'blog_posts.php?page=1', true);
+      xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+          var result = xhr.responseText;
+          console.log('Result: ' + result);
 
-          }
-        };
-        xhr.send();
-      }
+          hideSpinner();
+          // append results to end of blog posts
+          showLoadMore();
 
-      load_more.addEventListener("click", loadMore);
+        }
+      };
+      xhr.send();
+    }
 
-      // Load even the first page with Ajax
-      //loadMore();
-    </script>
+    load_more.addEventListener("click", loadMore);
 
-  </body>
+    // Load even the first page with Ajax
+    //loadMore();
+  </script>
+
+</body>
+
 </html>
