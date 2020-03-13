72952c189482ffec6ff5c444582aa8a42ada4c69
aliahmadcse <aliahmadcse@gmail.com>
2020-03-13 15:17:53 +0500

    Complete infinite scrolling project

--- Chapter_06/06_01/ajax_scroll/index.php
+++ Chapter_06/06_01/ajax_scroll/index.php
@@ -30,12 +30,13 @@
   </div>
 
   <div id="load-more-container">
-    <button id="load-more">Load more</button>
+    <button id="load-more" data-page="0">Load more</button>
   </div>
 
   <script>
     var container = document.getElementById('blog-posts');
     var load_more = document.getElementById('load-more');
+    var request_in_progress = false;
 
     function showSpinner() {
       var spinner = document.getElementById("spinner");
@@ -55,30 +56,70 @@
       load_more.style.display = 'none';
     }
 
-    function loadMore() {
+    function appendToDiv(div, new_html) {
+      //put the new element into a temp div
+      //this causes the browser to parse it as elements
+      const temp = document.createElement('div');
+      temp.innerHTML = new_html;
 
+      // use firstElementChild b/c this is how dom treats withspace
+      const class_name = temp.firstElementChild.className;
+      const items = temp.getElementsByClassName(class_name);
+
+      // console.log(items);
+      const len = items.length;
+      for (let i = 0; i < len; i++) {
+        div.appendChild(items[0]);
+      }
+    }
+
+    function setCurrentPage(page) {
+      console.log("incrementing the page to: " + page);
+      load_more.setAttribute('data-page', page);
+    }
+
+    function scrollReaction() {
+      let content_height = container.offsetHeight;
+      let current_y = window.innerHeight + window.pageYOffset;
+      // console.log(content_height + '/' + current_y);
+      if (current_y >= content_height) {
+        loadMore();
+      }
+    }
+
+    function loadMore() {
+      if (request_in_progress) {
+        return;
+      }
+      request_in_progress = true;
       showSpinner();
       hideLoadMore();
 
+      var page = parseInt(load_more.getAttribute('data-page'));
+      var next_page = page + 1;
+
       var xhr = new XMLHttpRequest();
-      xhr.open('GET', 'blog_posts.php?page=1', true);
+      xhr.open('GET', 'blog_posts.php?page=' + next_page, true);
       xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
       xhr.onreadystatechange = function() {
         if (xhr.readyState == 4 && xhr.status == 200) {
           var result = xhr.responseText;
-          console.log('Result: ' + result);
-
+          // console.log('Result: ' + result);
           hideSpinner();
+          setCurrentPage(next_page);
           // append results to end of blog posts
+          appendToDiv(container, result);
           showLoadMore();
-
+          request_in_progress = false;
         }
       };
       xhr.send();
     }
 
     load_more.addEventListener("click", loadMore);
-
+    window.onscroll = function() {
+      scrollReaction();
+    }
     // Load even the first page with Ajax
     loadMore();
   </script>
